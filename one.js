7cef6ed25f4a7edbffa31371863ba8d57f6fdf3c
Wiz2k20git <marcbrcx@gmail.com>
2022-10-22 23:40:50 -0300

    22/10/2022 23:40

---
+++ one.js
@@ -0,0 +1,3 @@
+document.getElementById('first').innerHTML = 'Número um!';
+document.getElementsByName('second')[0].innerHTML = 'Número dois!';
+document.getElementsByClassName('three')[0].innerHTML = 'Número três!';
